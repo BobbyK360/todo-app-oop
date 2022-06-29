20dbdbd8c0cddf65bc57808e8f2ad45bee67d0d5
Bobby K <bobbykamal@hotmail.co.uk>
2022-06-29 01:40:39 +0100

    more scss styling and some basic js functionality. More of a refresh, not representative of final OOP build

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,13 @@ import '../scss/index.scss';
 import testText from './partials/test';
 
 testText();
+
+//Really basic code to refresh myself on how to make a modal appear. Will not be in final OOP version.
+
+const addBtn = document.querySelector('.add-btn > .fa-square-plus');
+const todoModal = document.querySelector('.todo-modal');
+
+addBtn.addEventListener('click', () => {
+    console.log('clicked');
+    todoModal.classList.toggle('modal-appear');
+});
